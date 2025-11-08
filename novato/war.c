27205e2729285521398edcb2318eecd64b4ea6cf
well0x01 <77464680+well0x01@users.noreply.github.com>
2025-11-08 01:12:04 +0000

    Novo commit

--- novato/war.c
+++ novato/war.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#define QTD_TERRITORIOS 5
+#define TAM_NOME 50
+#define TAM_COR 30
+
+
+typedef struct {
+    char nome[TAM_NOME];
+    char corExercito[TAM_COR];
+    int numTropas;
+} Territorio;
+
+int main() {
+    Territorio territorios[QTD_TERRITORIOS];
+
+    printf("=== Cadastro de Territórios ===\n");
+
+    for (int i = 0; i < QTD_TERRITORIOS; i++) {
+        printf("\n--- Território %d ---\n", i + 1);
+
+        printf("Nome do território: ");
+        fgets(territorios[i].nome, TAM_NOME, stdin);
+        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0'; 
+
+        printf("Cor do exército: ");
+        fgets(territorios[i].corExercito, TAM_COR, stdin);
+        territorios[i].corExercito[strcspn(territorios[i].corExercito, "\n")] = '\0';
+
+        printf("Número de tropas: ");
+        scanf("%d", &territorios[i].numTropas);
+        getchar(); 
+    }
+
+    
+    printf("\n=== Estado Atual do Mapa ===\n");
+    for (int i = 0; i < QTD_TERRITORIOS; i++) {
+        printf("\nTerritório %d:\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor do Exército: %s\n", territorios[i].corExercito);
+        printf("Número de Tropas: %d\n", territorios[i].numTropas);
+    }
+
+    printf("\nCadastro concluído com sucesso!\n");
+    return 0;
+}
 
